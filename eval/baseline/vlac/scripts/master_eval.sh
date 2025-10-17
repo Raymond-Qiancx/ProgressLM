e322ddbd868276a3939f7cc64eb5f53bfa5e0033
Raymond-Qiancx <raymond.qiancx@gmail.com>
2025-10-17 12:46:12 -0500

    update

--- eval/baseline/vlac/scripts/master_eval.sh
+++ eval/baseline/vlac/scripts/master_eval.sh
@@ -22,21 +22,21 @@ cross_trajectory_ref=true
 
 # --- Path Configuration (MUST BE ABSOLUTE PATHS) ---
 # Path to the .jsonl file containing the dataset description.
-JSONL_PATH="/path/to/your/h5_tienkung_xsens_converted.jsonl"
+JSONL_PATH="/home/vcj9002/jianshu/workspace/code/ProgressLM/data/h5_tienkung_xsens_converted.jsonl"
 
 # Path to the root directory containing all image folders (e.g., h5_tienkung_xsens_1rgb/...).
-IMAGE_ROOT_DIR="/path/to/your/image_root_directory"
+IMAGE_ROOT_DIR="/home/vcj9002/jianshu/workspace/data/robomind/data/images"
 
 # Path to the downloaded VLAC model.
-MODEL_PATH="/path/to/your/VLAC/model"
+MODEL_PATH="/home/vcj9002/jianshu/workspace/weight/VLAC"
 
 # Directory where the final JSON results will be saved.
-OUTPUT_DIR="./results_pipeline"
+OUTPUT_DIR="/home/vcj9002/jianshu/workspace/data/robomind/data/annotations/cache"
 
 
 # --- GPU and Passthrough Arguments ---
 # Specify the GPU IDs to use, comma-separated (e.g., "0,1,4,7").
-GPU_IDS="0"
+GPU_IDS="0,1,2,3"
 
 # Other arguments to pass directly to the underlying run_eval.py script.
 # Example: --batch_num 10 --rich
@@ -54,12 +54,12 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PIPELINE_SCRIPT="$SCRIPT_DIR/../run_evaluation_pipeline.py"
 
 # Check if paths are placeholders
-if [[ "$JSONL_PATH" == "/path/to/your/h5_tienkung_xsens_converted.jsonl" ]] || \
-   [[ "$IMAGE_ROOT_DIR" == "/path/to/your/image_root_directory" ]] || \
-   [[ "$MODEL_PATH" == "/path/to/your/VLAC/model" ]]; then
-    echo -e "\033[0;31m[ERROR] Please update the placeholder paths in this script before running.\033[0m"
-    exit 1
-fi
+# if [[ "$JSONL_PATH" == "/path/to/your/h5_tienkung_xsens_converted.jsonl" ]] || \
+#    [[ "$IMAGE_ROOT_DIR" == "/path/to/your/image_root_directory" ]] || \
+#    [[ "$MODEL_PATH" == "/path/to/your/VLAC/model" ]]; then
+#     echo -e "\033[0;31m[ERROR] Please update the placeholder paths in this script before running.\033[0m"
+#     exit 1
+# fi
 
 # Build the command
 CMD="python $PIPELINE_SCRIPT \
